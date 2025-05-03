eb4a414e335a12e2c2413e0a61721c29ae49a153
ozturkomerfaruk <omerfarukozturk026@gmail.com>
2025-05-03 20:31:44 +0300

    AIChat

---
+++ AIChat/AIChat/AIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatApp.swift
+//  AIChat
+//
+//  Created by Ömer Faruk Öztürk on 3.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct AIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
