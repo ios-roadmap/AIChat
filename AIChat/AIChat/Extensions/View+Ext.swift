5792d793d1858969e23554a369836f5a37735c9e
ozturkomerfaruk <omerfarukozturk026@gmail.com>
2025-05-19 12:43:44 +0300

    CategoryCell

--- AIChat/AIChat/Extensions/View+Ext.swift
+++ AIChat/AIChat/Extensions/View+Ext.swift
@@ -21,4 +21,25 @@ extension View {
     func tappableBackground() -> some View {
         background(Color.black.opacity(0.01))
     }
+    
+    func removeListRowFormatting() -> some View {
+        self
+            .listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
+            .listRowBackground(Color.clear)
+    }
+    
+    func addingGradientBackgroundForText() -> some View {
+        self
+            .background(
+                LinearGradient(
+                    colors: [
+                        Color.black.opacity(0),
+                        Color.black.opacity(0.3),
+                        Color.black.opacity(0.4)
+                    ],
+                    startPoint: .top,
+                    endPoint: .bottom
+                )
+            )
+    }
 }
