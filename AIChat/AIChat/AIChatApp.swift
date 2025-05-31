090ced4b047429d803673b3518f6940e9ee6fe6b
ozturkomerfaruk <omerfarukozturk026@gmail.com>
2025-05-31 16:37:57 +0300

    Add Firebase

--- AIChat/AIChat/AIChatApp.swift
+++ AIChat/AIChat/AIChatApp.swift
@@ -9,9 +9,21 @@ import SwiftUI
 
 @main
 struct AIChatApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             AppView()
         }
     }
+}
+
+import Firebase
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
 }
